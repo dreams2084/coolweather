73727479acd1e44320d7e8e50f3d09ce69b6bd1d
apple <apple@MacBook-Pro-2.local>
2020-01-02 15:35:46 +0800

    第一次加入了显示天气信息的功能

--- app/src/main/java/com/coolweather/android/util/Utility.java
+++ app/src/main/java/com/coolweather/android/util/Utility.java
@@ -5,6 +5,8 @@ import android.text.TextUtils;
 import com.coolweather.android.db.City;
 import com.coolweather.android.db.County;
 import com.coolweather.android.db.Province;
+import com.coolweather.android.gson.Weather;
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -86,7 +88,7 @@ public class Utility {
     /**
      * 将返回的JSON数据解析成weather实体类
      */
-    /*public static Weather handleWeatherResponse(String response) {
+    public static Weather handleWeatherResponse(String response) {
         try {
             JSONObject jsonObject = new JSONObject(response);
             JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
@@ -96,7 +98,7 @@ public class Utility {
             e.printStackTrace();
         }
         return null;
-    }*/
+    }
 
 
 }
